1529868938d3360efe37e04fccaa417409fb812f
Rick Mason <richard.mason@eastsussex.gov.uk>
2013-11-05 14:02:47 +0000

    Update interfaces to allow read-only repositories

--- IGritterDataRepository.cs
+++ IGritterDataRepository.cs
@@ -1,19 +1,12 @@
 ﻿
-using System.Collections.Generic;
 
 namespace Escc.Gritting
 {
     /// <summary>
     /// A service to store and retrieve data about gritters
     /// </summary>
-    public interface IGritterDataRepository
+    public interface IGritterDataRepository : IGritterDataSource
     {
-        /// <summary>
-        /// Reads data about all gritters including their current position
-        /// </summary>
-        /// <returns></returns>
-        ICollection<Gritter> ReadAllGritters();
-
         /// <summary>
         /// Adds or updates data about a gritter
         /// </summary>
